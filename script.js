5722822667b0dccb55472f3bf988af7edc25ec1d
ipradana <ipradana@telefonica.net>
2025-08-01 13:23:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-s// script.js
+oss// script.js
 
 // --- Definición de Dados ---
 const nombresDeDados = [
-    "Conceptos", "Autores", "Acciones 1", "Ismos", "Conceptos II", 
+    "Conceptos", "Autores", "Acciones", "Objetos", "Conceptos II", 
     "Ética", "Política", "Metáforas", "Autores II"
 ];
 
@@ -13,8 +13,8 @@ function crearRutaImagen(texto) {
 
 const dado1_conceptos = ["imagenes/dios.png", "imagenes/icon1.png", "imagenes/libertad.png", "imagenes/tiempo.png", "imagenes/destino.png", "imagenes/esencia.png"];
 const dado2_autores = ["imagenes/platon.jpg", "imagenes/aristoteles.jpg", "imagenes/kant.jpg", "imagenes/nietzsche.jpg", "imagenes/descartes.png", "imagenes/wittgenstein.jpg"];
-const dado3_preguntas = ["imagenes/acciones1/caminar.pgn", "imagenes/acciones1/construir.png", "imagenes/acciones1/saltar.png", "imagenes/acciones1/mirar.pgn", "imagenes/acciones1/pensar.png", "imagenes/acciones1/toser.pgn"];
-const dado4_ismos = ["Racionalismo", "Empirismo", "Existencialismo", "Estoicismo", "Idealismo", "Materialismo"].map(crearRutaImagen);
+const dado3_preguntas = ["imagenes/acciones1/caminar.png", "imagenes/acciones1/construir.png", "imagenes/acciones1/saltar.png", "imagenes/acciones1/mirar.pgn", "imagenes/acciones1/pensar.png", "imagenes/acciones1/toser.pgn"];
+const dado4_ismos = ["imagenes/objetos1/arcoriris.png", "imagenes/objetos1/balanza.png", "imagenes/objetos1/candado.png", "imagenes/objetos1/llave.png", "imagenes/objetos1/lupa.png", "imagenes/objetos1/manzana.png"];
 const dado5_conceptos2 = ["Alma", "Cuerpo", "Mente", "Realidad", "Virtualidad", "Lenguaje"].map(crearRutaImagen);
 const dado6_etica = ["El Bien", "El Mal", "Virtud", "Deber", "Felicidad", "Utilitarismo"].map(crearRutaImagen);
 const dado7_politica = ["Estado", "Poder", "Ley", "Ciudadano", "Democracia", "Contrato Social"].map(crearRutaImagen);
